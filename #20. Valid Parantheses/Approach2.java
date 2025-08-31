6830b561a800091c1b78e17d4cb00ba1d11d2880
NayanSaraff <nayansaraff3739@gmail.com>
2025-09-01 00:36:07 +0530

    Create Approach2.java

---
+++ #20. Valid Parantheses/Approach2.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+
+        for(int i=0;i<s.length();i++)
+        {
+            char ch = s.charAt(i);
+
+            if(ch == '(') st.push(')');
+            else if(ch == '{') st.push('}');
+            else if(ch == '[') st.push(']');
+            else
+            {
+                if(st.isEmpty()|| ch!=st.pop())
+                return false;
+            }
+        }
+        return st.isEmpty();
+    }
+}
